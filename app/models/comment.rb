10e579e4b0b033e6034cd6d3fba1e82f61d8bfa0
Michelle Olivieri <michelleo72@gmail.com>
2018-06-06 22:13:13 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ActiveRecord::Base
   accepts_nested_attributes_for :user, reject_if: :username.blank?
 
   def user_attributes=(user_attributes)
+    if user_attributes[:username] != ""
     self.user = User.find_or_create_by(user_attributes)
+  end
   end
 end
