941f566f628540d02933fdf379123b4d1ecf1095
Michelle Olivieri <michelleo72@gmail.com>
2018-06-06 19:45:29 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,13 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post  = Post.create(post_params)
+    redirect_to post
+  end
 end
