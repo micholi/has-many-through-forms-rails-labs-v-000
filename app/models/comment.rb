37391cc23b010c6997aa3691da9a2a3840ac46d2
Michelle Olivieri <michelleo72@gmail.com>
2018-06-06 21:45:46 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: :username.blank?
+
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_att|
+      user = User.find_or_create_by(user_att)
 end
