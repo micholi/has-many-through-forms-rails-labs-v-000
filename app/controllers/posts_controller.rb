6d1313ff22b6ef8036dc59c9439386dd2fb09198
Michelle Olivieri <michelleo72@gmail.com>
2018-06-06 22:13:58 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,7 @@
 class PostsController < ApplicationController
+
   def show
     @post = Post.find(params[:id])
-  end
-
-  def index
-    @posts = Post.all
   end
 
   def new
@@ -12,8 +9,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create(post_params)
-    redirect_to post
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :username.blank?
 
+  def user_attributes=(user_attributes)
+    if user_attributes[:username] != ""
+    self.user = User.find_or_create_by(user_attributes)
+  end
+  end
 end
