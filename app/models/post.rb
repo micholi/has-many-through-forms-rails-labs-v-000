be8f5acec02afe7dea2b2341ee467ba22d7a41e6
Michelle Olivieri <michelleo72@gmail.com>
2018-06-06 20:37:55 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
 
-  def categories_attributes=(category_attributes)
-    category_attributes.values.each do |cat_att|
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |cat_att|
       category = Category.find_or_create_by(cat_att)
       self.categories << category
     end
